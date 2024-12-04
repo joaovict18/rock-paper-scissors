fde09a4e3767351e27d50720e72553d7beb820d9
João Victor <106018733+joaovict18@users.noreply.github.com>
2024-12-04 14:31:14 -0300

    Almost done, but it lasts a logic problem in playRound function

--- app.js
+++ app.js
@@ -1,14 +1,56 @@
-let choices = ["rock", "paper", "scissors"];
-let randomChoice = Math.floor(Math.random() * choices.length);
+const choices = ["rock", "paper", "scissors"];
+let randomChoice = Math.round(Math.random() * 2);
 
 function getComputerChoice() {
     return choices[randomChoice];
 }
 
-
+let choice;
 
 function getHumanChoice() {
-    
+    choice = prompt("Make your choice: ").toLowerCase();
+    return choice;
 }
 
-console.log(getComputerChoice());
+let computerSelection = getComputerChoice();
+let humanSelection = getHumanChoice();
+
+let computerScore = 0
+let humanScore = 0;
+
+function playRound(computerChoice, humanChoice) {
+    if (computerChoice == "rock" && humanChoice == "paper") {
+        console.log("You win!");
+        humanScore++;
+    }
+    else if (computerChoice == "paper" && humanChoice == "scissors") {
+        console.log("You win!");
+        humanScore++;
+    }
+    else if (computerChoice == "scissors" && humanChoice == "rock") {
+        console.log("You win!");
+        humanScore++;
+    }
+    else if (computerChoice == "paper" && humanChoice == "rock") {
+        console.log("You loose.");
+        computerScore++;
+    }
+    else if (computerChoice == "scissors" && humanChoice == "paper") {
+        console.log("You loose.");
+        computerScore++;
+    }
+    else if (computerChoice == "rock" && humanChoice == "scissors"){
+        console.log("You loose.");
+        computerScore++;
+    }
+    else {
+        console.log("Withdraw.");
+    }
+}
+
+// problem bellow
+for (let i=0; i<3; i++) {
+    playRound(computerSelection, humanSelection);
+}
+
+console.log(`Final score:\nComputer: ${computerScore}\nHuman: ${humanScore}`);
